3233a154df9e73b6621bf6f95cc6b5db9d699117
Hasan Rafi Ahmed <hasan.cse570@gmail.com>
2026-04-18 11:53:26 +0600

    real problem setion add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import KeyBenefits from "@/components/home/KeyBenefits";
+import SolveRealProblems from "@/components/home/SolveRealProblems";
 
 export default function Home() {
   return (
     <div className="bg-black pt-20">
+      <SolveRealProblems></SolveRealProblems>
       <KeyBenefits />
     </div>
   );
---
+++ src/components/home/SolveRealProblemCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SolveRealProblemCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SolveRealProblemCard;
