213e438c305b2b7fd1afba2be48f4eeea4c5045d
omar-faruk72 <mdfaruk01720875872@gmail.com>
2026-04-20 16:04:50 +0600

    added ann layout error solve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,13 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Navbar />
         {/* Added top padding to account for fixed navbar */}
-<<<<<<< HEAD
-        <main className="flex-grow    mx-auto w-full">
-          {children}
-        </main>
-=======
         <main className="flex-grow  pb-12  mx-auto w-full">{children}</main>
->>>>>>> d64a76d (dev fixter icon and interatins section update)
         <Footer />
       </body>
     </html>
